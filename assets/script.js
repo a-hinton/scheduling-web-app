45851ca570c6fe4d0585a7c52417f0a8a87aed6c
a-h <alexandra.k.hinton1@gmail.com>
2020-11-12 13:33:17 -0500

    try rendering DOM elements in JS

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+// Initialize page
+let currentDate = moment().format('dddd' + ', ' + 'LL');
+document.getElementById("currentDay").innerHTML = currentDate;
+console.log(currentDate);
+
+
+let times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+
+// grab html elements
+
+// functions!
+function taskInput(event) {
+    
+}
+
+function timeCheck() {
+    if (past) {
+        color white
+    }
+    else if (current) {
+        color red
+    }
+    else {
+        color green
+    }
+}
+
+// event listeners
+addEventListener()
+
+// local storage?
