89accf8e336f427c856fc8878becff83e2be2bef
a-h <alexandra.k.hinton1@gmail.com>
2020-11-14 16:38:43 -0500

    update dynamic element rendering

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# scheduling-web-app
+# Scheduling Website
+
+## Developer:
+Website made by Alexandra Hinton.
+
+Find me on GitHub and LinkedIn:
+
+* https://github.com/a-hinton
+
+* www.linkedin.com/in/alexandrakhinton
+
+## Details:
+This site uses JQuery to dynamically render HTML elements to the DOM.
+
+## Screenshots:
+![landing-page](./assets/images/landing-page.png)
+![demo-tasks](./assets/images/demo-task.png)
--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,75 @@
+// Declare variables
+let times = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+let currentTime = moment().format('HH');
+
+
 // Initialize page
-let currentDate = moment().format('dddd' + ', ' + 'LL');
-document.getElementById("currentDay").innerHTML = currentDate;
-console.log(currentDate);
-
-
-let times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-
-
-// grab html elements
+$(document).ready(function() {
+    let currentDate = moment().format('dddd' + ', ' + 'LL');
+    document.getElementById("currentDay").innerHTML = currentDate;
+    renderTimeBlocks();
+    // Retrieve local storage data
+    let todos = localStorage.getItem("storedTodos") ? JSON.parse(localStorage.getItem("storedTodos")) : []
+    todos.forEach(function(todo, i){
+    if (todo.task){
+        $(`#${todo.time}`).text(todo.task)
+    }
+    console.log(todo.time)
+    })
+})
 
 // functions!
-function taskInput(event) {
-    
-}
+function renderTimeBlocks() {
+    times.forEach(function(time, i){
+        let addedElement = times[i];
+        // dynamically render a row with styling
+        let row = $("<div/>",{
+            "class" : "row time-block col-12 hour",
+            "id" : addedElement + "row"
+        }).appendTo(".container");
 
-function timeCheck() {
-    if (past) {
-        color white
-    }
-    else if (current) {
-        color red
-    }
-    else {
-        color green
-    }
+        // dynamically render a column for the hour and append to row
+        let hour = $("<div>", {
+            "class" : "hour col-2",
+            "text" : addedElement,
+        }).appendTo(row);
+
+        // Determine relative time relationship beween current time and the time block being rendered and append to row
+        if(currentTime === addedElement) {
+            let textArea = $("<textarea>", {
+                "class" : "present description col-9",
+                "id" : addedElement
+            }).appendTo(row);
+        }
+        else if (currentTime > addedElement) {
+            let textArea = $("<textarea>", {
+                "class" : "past description col-9",
+                "id" : addedElement
+            }).appendTo(row);
+        }
+        else {
+            let textArea = $("<textarea>", {
+                "class" : "future description col-9",
+                "id" : addedElement
+            }).appendTo(row);
+        };
+        
+        // render save button and append to row
+        let saveBtn = $("<button>", {
+            "class" : "saveBtn col-1",
+            "text" : "Save",
+        }).appendTo(row);
+        i++;
+        console.log(i)
+    });
 }
 
 // event listeners
-addEventListener()
-
-// local storage?
+$(document).on("click", ".saveBtn", function() {
+    let savedTask = {
+        task: $(this).siblings(".description").val(),
+        time: $(this).siblings(".description").attr("id"),
+    }
+    todos.push(savedTask);
+    localStorage.setItem("storedTodos", JSON.stringify(todos))
+})
